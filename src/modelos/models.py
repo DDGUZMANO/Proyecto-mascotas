9fd55dda499975e161ce9434f06bf6df941a2fd7
DOUGUZMANO <douglasguzmanorence29@gmail.com>
2025-03-21 13:19:22 +0100

    modificacion en tests_models, creacion de __init__

---
+++ src/modelos/models.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+@dataclass
+
+class Usuario:
+    id_usuario: int
+    nombre: str
+    ubicacion: str
+    tipo_mascota_preferida: str = None 
+
+@dataclass
+class Cuidador:
+    id_cuidador: int
+    nombre: str
+    ubicacion: str
+    tipo_mascota_aceptada: str
+    tamaño_mascota_aceptada: str
+    experiencia: int
+
+@dataclass
+class Mascota:
+    id_mascota: int
+    nombre: str
+    tipo_mascota: str
+    tamaño: str
+    necesidades_especiales: bool
+    id_usuario: int #Clave foránea que relaciona a la mascota con el usuario
